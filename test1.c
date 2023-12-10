4389e9cc51207132277b6153da17480d140bc806
Yu <userszy@163.com>
2023-12-10 12:11:28 +0800

    新建inv_data结构体

--- test1.c
+++ test1.c
@@ -2,7 +2,7 @@
  * @Author: Yu userszy@163.com
  * @Date: 2023-12-10 11:18:34
  * @LastEditors: Yu userszy@163.com
- * @LastEditTime: 2023-12-10 11:23:12
+ * @LastEditTime: 2023-12-10 12:10:32
  * @FilePath: /learngit/test1.c
  * @Description: 
  * 
@@ -10,10 +10,16 @@
  */
 #include <stdio.h>
 
-int main(){
+int main()
+{
     int a = 10;
     int b = 20;
     int c = a + b;
     printf("c = a + b the result is :%d\r\n",c);
     return 0;
 }
+
+struct inv_data
+{
+    /* data */
+};
