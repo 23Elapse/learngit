03d96d73b2765d38278cc095b5757ee445bc1993
Yu <userszy@163.com>
2023-12-10 11:29:29 +0800

    add test1.c and vscode config files

---
+++ test1.c
@@ -0,0 +1,19 @@
+/*
+ * @Author: Yu userszy@163.com
+ * @Date: 2023-12-10 11:18:34
+ * @LastEditors: Yu userszy@163.com
+ * @LastEditTime: 2023-12-10 11:23:12
+ * @FilePath: /learngit/test1.c
+ * @Description: 
+ * 
+ * Copyright (c) 2023 by ${git_name_email}, All Rights Reserved. 
+ */
+#include <stdio.h>
+
+int main(){
+    int a = 10;
+    int b = 20;
+    int c = a + b;
+    printf("c = a + b the result is :%d\r\n",c);
+    return 0;
+}
